f68f859921ac708702570a2161988a18bbc39bc2
Shadow7x <loginvlad1000@gmail.com>
2025-05-14 21:04:57 +0400

    superUser and migrations

--- UpTrader/main/admin.py
+++ UpTrader/main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name','menu_name','parent','get_url','order')
+    list_filter = ('menu_name',)
+    search_fields = ('name', 'url', 'named_url')
+    
+    def get_url(self, obj):
+        return obj.get_url
+    get_url.short_description = 'URL'
+    
